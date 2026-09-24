c879d41eab30179cc480d916154a26e82f2a9a21
vicktorz199-oss <vicktorz199@gmail.com>
2026-09-24 12:34:26 +0300

    Add files via upload
    test soft added

---
+++ ctl_websock_C02/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from core.mainshell import MainShell
+
+if __name__ == '__main__':
+	MainShell().cmdloop()
---
+++ ctl_websock_C02/test.cmd
@@ -0,0 +1,6 @@
+run net_get.json
+run pad_get.json
+run reader_get.json
+run exdev_get.json
+run exdev_control_open.json
+run exdev_control_open_1.json
